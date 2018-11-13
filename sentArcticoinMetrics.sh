feca397b3d317cd4e434e38a0dfcf4860a8981f0
Rafal <rafal.sladek@gmail.com>
2018-11-14 00:41:39 +0100

    add sentArcticoinMetrics.sh

---
+++ sentArcticoinMetrics.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#set -xe
+source /usr/local/bin/ddhelper.sh
+
+host=$(hostname)
+coin="arcticcoin"
+role="masternode"
+coindaemon="arcticcoind"
+coincli="arcticcoin-cli"
+
+metricname="blocks"
+value=$(/usr/local/bin/$coincli getblockcount)
+    sentMetric $host $coin $metricname $value $role
+
+metricname="connections"
+value=$(/usr/local/bin/$coincli getconnectioncount)
+    sentMetric $host $coin $metricname $value $role
+
+metricname="enabled"
+publicip=$(curl -s ipecho.net/plain)
+value=$(/usr/local/bin/$coincli goldminenode list full | /bin/grep $publicip | /bin/grep -w ENABLED | /bin/grep -v PRE-ENABLED | /usr/bin/wc -l)
+    sentMetric $host $coin $metricname $value $role
+
+metricname="status"
+value=$(/usr/local/bin/$coincli goldminenode status | /bin/grep status | /usr/bin/awk -F'"' '{printf "%s",$4}' | /usr/bin/tr -d ",")
+success=0
+if [[ 'Goldminenode successfully started' == $value ]]; then
+    success=1
+fi
+    sentMetric $host $coin $metricname $success $role
