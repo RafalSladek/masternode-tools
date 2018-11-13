d8084a36412a0922d9f304935fb43f28bc0645b4
Rafal <rafal.sladek@gmail.com>
2018-11-14 00:16:03 +0100

    add datadog helper function sentMetric

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # masternode-tools
 shell scripts for monitoring my masternodes
+
+## Setup
+1. You need to export system/profile env variable
+```export DATADOG_API_KEY=<YOUR_DATADOG_API_HERE>```
+
+2. clone this repo into `<YOUR_DIR>`, for example `/usr/local/src`
+
+## How to use the datadog helper function `sentMetric()`
+
+```
+source <YOUR_DIR>/masternode-tools/ddhelper.sh
+
+sentMetric $host $coin $metricname $value $role
+```
---
+++ ddhelper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -xe
+
+function sentMetric() {
+
+if [ -z "$1" ]
+then
+echo "ERROR: Please specify the host as the first argument."
+else
+	if [ -z "$2" ]
+   	then
+    echo "ERROR: Please specify the coin as the second argument."
+	else
+		if [ -z "$3" ]
+		then
+		echo "ERROR: Please specify the metricname as the third argument."
+        else
+			if [ -z "$4" ]
+			then
+			echo "ERROR: Please specify the value as the fourth argument."
+			else
+				if [ -z "$5" ]
+				then
+				echo "ERROR: Please specify the role as the fifth argument."
+				else
+					currenttime=$(/bin/date +%s)
+					/usr/bin/curl --silent -X POST -H "Content-type: application/json" \
+					-d "{ \"series\" :
+							[{\"metric\":\"$5.$3\",
+							\"points\":[[$currenttime, $4]],
+							\"type\":\"gauge\",
+							\"host\":\"$1\",
+							\"tags\":[\"coin:$2\", \"role:$5\"]}
+							]
+					}" \
+					'https://app.datadoghq.com/api/v1/series?api_key=$DATADOG_API_KEY' > /dev/null
+				fi
+			fi
+		fi		
+	fi
+fi
+}
