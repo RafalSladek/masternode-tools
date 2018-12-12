8eccf6b6abaf3662e7e0ad9d304e1ec97afdef71
Rafal Sladek <rafal.sladek@gmail.com>
2018-12-12 02:25:35 +0100

    improve setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+set -xuo
 
 echo "setting up everthing ..."
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+TARGETPATH=/usr/local/bin
 
 for i in `ls -l | awk '{ if ( $1 ~ /x/ ) {print $NF}}'`; 
-    do [ -x $i ] && echo "linking $SCRIPTPATH/$i ..."; 
+    do
+    if [ -x $i ]
+	then
+        rm -rf $TARGETPATH/$i && echo "removed old $TARGETPATH/$i"
+        ln -s $SCRIPTPATH/$i $TARGETPATH/$i && echo "linked new $SCRIPTPATH/$i"
+    fi
 done
+
+cd $TARGETPATH && ls -lhaF | grep ^l
