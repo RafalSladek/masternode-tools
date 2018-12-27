6c9acf306d5d5f8bd359816ec101f29c3e33e836
Rafal Sladek <rafal.sladek@gmail.com>
2018-12-27 18:09:26 +0100

    fix the else path

--- resetArcticChain.sh
+++ resetArcticChain.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
 set -xue
-
 source /usr/local/bin/tools.sh
 
 COINUSER=arcticcoin
 COINEXPLORER=http://explorer.arcticcoin.org/api/getblockcount
 
 function wipeArcticoinChain() {
+    #runCommandWithUser $COINUSER 'arcticcoin-cli stop'
+    #runCommandWithUser $COINUSER 'arcticcoind -rescan'
+    #runCommandWithUser $COINUSER 'arcticcoind -reindex'
     cd /home/arcticcoin/.arcticcore
     ls -al
     sudo rm -vf *.log *.dat .lock
@@ -21,21 +22,7 @@ globalBlock=$(curl -sk "$COINEXPLORER")
 
 if [[ "$localBlock" == "$globalBlock" ]]; then
     echo "Yeee, your arcticcoin is in sync"
-    runCommandWithUser $COINUSER 'arcticcoin-cli stop'
-    sudo systemctl start arcticcoin
     arcticstatus
 else
-    runCommandWithUser $COINUSER 'arcticcoin-cli stop'
-    runCommandWithUser $COINUSER 'arcticcoind -rescan'
-    #runCommandWithUser $COINUSER 'arcticcoind -reindex' 
-    arcticstatus
-    localBlock=$(runCommandWithUser $COINUSER 'arcticcoin-cli getblockcount')
-    globalBlock=$(curl -sk "$COINEXPLORER")
-    if [[ "$localBlock" == "$globalBlock" ]]; then
-        echo "Yeee, your arcticcoin is in sync after second try"
-        runCommandWithUser $COINUSER 'arcticcoin-cli stop'
-        sudo systemctl start arcticcoin
-    else
-        echo "hard way..."
-    fi
+    wipeArcticoinChain
 fi
