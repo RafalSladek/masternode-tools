f7d999db34cb6e7ae9fb8592cbaaec55cc3013d2
Rafal Sladek <rafal.sladek@gmail.com>
2019-01-25 20:58:46 +0100

    add skip to rootkit hunter

--- upgradeMasterNode.sh
+++ upgradeMasterNode.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
 sudo rkhunter --propupd
-sudo rkhunter -c --enable all --disable none
+sudo rkhunter -c --enable all --disable none --sk
--- upgradeSecureZenNode.sh
+++ upgradeSecureZenNode.sh
@@ -6,7 +6,7 @@ sudo apt -y autoremove
 sudo apt-get clean
 sudo apt-get autoclean
 sudo rkhunter --propupd
-sudo rkhunter -c --enable all --disable none
+sudo rkhunter -c --enable all --disable none --sk
 sudo systemctl restart zen
 sleep 80s && zenstatus
 /usr/bin/certbot certificates
