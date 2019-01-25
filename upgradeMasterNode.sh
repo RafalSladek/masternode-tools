fd16c03dd229322c58ff77ae65cbf7a25853bbac
Rafal Sladek <rafal.sladek@gmail.com>
2019-01-25 18:22:42 +0100

    add upgrade script

---
+++ upgradeMasterNode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+sudo rkhunter --propupd
+sudo rkhunter -c --enable all --disable none
---
+++ upgradeSecureZenNode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install zen -y
+sudo apt -y dist-upgrade
+sudo apt -y autoremove
+sudo apt-get clean
+sudo apt-get autoclean
+sudo rkhunter --propupd
+sudo rkhunter -c --enable all --disable none
+sudo systemctl restart zen
+sleep 80s && zenstatus
+/usr/bin/certbot certificates
+/usr/bin/certbot renew
+checkzenservices
