dd2846af6ad521c6d3a86274d785d41583e3503e
Rafal Sladek <rafal.sladek@gmail.com>
2019-03-15 22:41:23 +0100

    add rsyncArcticcoreWith.sh

---
+++ rsyncArcticcoreWith.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function rsyncArcticCoreWith() {
+    
+    SOURCE=/home/arcticcoin/.arcticcore
+    TARGET=/tmp
+    TARGET_IP=$1
+    TARGET_USER=rsyncuser
+    EXCLUDE_FILE=exclude-list.txt
+  cat << EOF > $EXCLUDE_FILE
+*.log
+wallet.dat
+banlist.dat
+fee_estimates.dat
+gmcache.dat
+gmpayments.dat
+governance.dat
+netfulfilled.dat
+peers.dat
+*.pid
+*.conf
+.lock
+backups
+EOF
+    
+    
+    /usr/bin/rsync -auvP --exclude-from $EXCLUDE_FILE --no-compress --timeout=60 -e "ssh" --rsync-path="sudo rsync" $SOURCE $TARGET_USER@${TARGET_IP}:$TARGET
+}
+
+rsyncArcticCoreWith '81.169.223.222'
+rsyncArcticCoreWith '107.150.7.213'
