87231bf92fd98b7e442acc035bc61ab6231088fd
Rafal <rafal.sladek@gmail.com>
2019-03-26 22:11:26 +0100

    refactor main function

--- arcticSoftReset.sh
+++ arcticSoftReset.sh
@@ -10,19 +10,23 @@ function copying() {
     cd $COINPATH
     echo "coin daemon $COINUSER is copying and linking ..."
     runCommandWithUser $COINUSER "cd $COINPATH && rm -rf * && rm -rf .lock"
-    runCommandWithUser $COINUSER "cd $COINPATH &&  cp -r /tmp/.arcticcore/* ."
+    runCommandWithUser $COINUSER "cd $COINPATH && cp -r /tmp/.arcticcore/* ."
     runCommandWithUser $COINUSER "cd $COINPATH && ln -s ../arcticcoin.conf"
 }
 
-localBlock=$(runCommandWithUser $COINUSER 'arcticcoin-cli getblockcount')
-globalBlock=$(curl -sk "$COINEXPLORER")
+function main(){
+    localBlock=$(runCommandWithUser $COINUSER 'arcticcoin-cli getblockcount')
+    globalBlock=$(curl -sk "$COINEXPLORER")
 
-if [[ "$localBlock" == "$globalBlock" ]]; then
-    echo "Yeee, your arcticcoin is in sync"
-    arcticstatus
-else
-    echo "local blocks:  $localBlock"
-    echo "global blocks: $globalBlock"
-    sudo systemctl stop arcticcoin && copying && sudo systemctl start arcticcoin
-    watch -d arcticstatus
-fi
+    if [[ "$localBlock" == "$globalBlock" ]]; then
+        echo "Yeee, your arcticcoin is in sync"
+        arcticstatus
+    else
+        echo "local blocks:  $localBlock"
+        echo "global blocks: $globalBlock"
+        sudo systemctl stop arcticcoin && copying && sudo systemctl start arcticcoin
+        watch -d arcticstatus
+    fi
+}
+
+main()
