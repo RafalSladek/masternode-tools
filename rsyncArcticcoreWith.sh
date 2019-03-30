be41662ce318adb22b902a4ad9d62865db782821
Rafal Sladek <rafal.sladek@gmail.com>
2019-03-30 09:22:05 +0100

    change from exclude to include

--- rsyncArcticcoreWith.sh
+++ rsyncArcticcoreWith.sh
@@ -6,35 +6,22 @@ function rsyncArcticCoreWith() {
     TARGET=/tmp
     TARGET_IP=$1
     TARGET_USER=rsyncuser
-    EXCLUDE_FILE=exclude-list.txt
-  cat << EOF > $EXCLUDE_FILE
-debug.log
-db.log
-wallet.dat
-banlist.dat
-fee_estimates.dat
-gmcache.dat
-gmpayments.dat
-governance.dat
-netfulfilled.dat
-peers.dat
-arcticcoin.conf
-goldminenode.conf
-arcticcoind.pid
-.lock
-backups
+    INCLUDE_FILE=include-list.txt
+  cat << EOF > $INCLUDE_FILE
+blocks
+chainstate
+database
 EOF
     
     /usr/bin/rsync \
         --log-file=/var/log/rsyncArcticCoreWith.log \
-        --quiet \
         --archive \
         --safe-links \
         --checksum \
         --ignore-times \
         --progress \
         --stats \
-        --exclude-from $EXCLUDE_FILE \
+        --include-from= $INCLUDE_FILE \
         --timeout=60 \
         -e "ssh" \
         --rsync-path="sudo rsync" $SOURCE $TARGET_USER@${TARGET_IP}:$TARGET
