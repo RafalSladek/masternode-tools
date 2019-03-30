01a393b9bca87dc3c52009216d015fd56c2bab14
Rafal Sladek <rafal.sladek@gmail.com>
2019-03-30 15:36:53 +0100

    refactoring

--- rsyncArcticcoreWith.sh
+++ rsyncArcticcoreWith.sh
@@ -6,26 +6,52 @@ function rsyncArcticCoreWith() {
     TARGET=/tmp
     TARGET_IP=$1
     TARGET_USER=rsyncuser
+    EXCLUDE_FILE=exclude-list.txt
     INCLUDE_FILE=include-list.txt
-  cat << EOF > $INCLUDE_FILE
-blocks
-chainstate
-database
+
+cat << EOF > $EXCLUDE_FILE
+*wallet.dat*
+*.conf
+debug.log
+db.log
+backups*
+.lock
+*.pid
+*banlist.dat*
+*fee_estimates.dat*
+*gmcache.dat*
+*governance.dat*
+*gmpayments.dat*
+*netfulfilled.dat*
+*peers.dat*
+EOF
+
+cat << EOF > $INCLUDE_FILE
+chainstate*
+blocks*
+database*
 EOF
     
     /usr/bin/rsync \
-        --log-file=/var/log/rsyncArcticCoreWith.log \
-        --archive \
-        --safe-links \
-        --checksum \
-        --ignore-times \
-        --progress \
-        --stats \
-        --include-from= $INCLUDE_FILE \
-        --timeout=60 \
-        -e "ssh" \
-        --rsync-path="sudo rsync" $SOURCE $TARGET_USER@${TARGET_IP}:$TARGET
+    --log-file=/var/log/rsyncArcticCoreWith.log \
+    --archive \
+    --progress \
+    --human-readable \
+    --stats \
+    --delete-excluded \
+    --exclude-from=$EXCLUDE_FILE \
+    --timeout=60 \
+    -e "ssh" \
+    --rsync-path="sudo rsync" $SOURCE $TARGET_USER@${TARGET_IP}:$TARGET
 }
 
-rsyncArcticCoreWith '81.169.223.222'
-rsyncArcticCoreWith '107.150.7.213'
+function main(){
+    ips=('81.169.223.222' '107.150.7.213')
+    for ip in "${ips[@]}"
+    do
+        echo "rsync starting for $ip ..."
+        rsyncArcticCoreWith $ip
+    done
+}
+
+main
