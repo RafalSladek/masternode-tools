366a1b511b2cc8a48391d2845a13302a5bc5d801
Rafal Sladek <rafal.sladek@gmail.com>
2019-07-17 16:03:04 +0200

    add explorer metric for zen

---
+++ sentExplorerZencoinMetrics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#set -xe
+source /usr/local/bin/ddhelper.sh
+
+host=$(hostname)
+coin="zen"
+role="masternode"
+username=$(whoami)
+coinexplorerurl=https://explorer.horizen.global/insight-api-zen/sync
+
+highestBlock=$(curl -sk $coinexplorerurl | jq .blockChainHeight)
+metricname="explorer.blocks"
+value=$highestBlock
+sentMetric $host $coin $metricname $value $role $username
