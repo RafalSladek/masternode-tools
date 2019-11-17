b8a5189140a3531bb4c1a9d245d5858d56faffe7
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-18 00:10:45 +0100

    add crop clean script

---
+++ cleanCropcoin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    COIN=cropcoin
+else
+    COIN=$1
+fi
+USER=$COIN
+COINDIR=.cropcoin
+COINSTATUSCMD=cropstatus
+
+echo stoping service $COIN ...
+systemctl stop $COIN
+
+echo removing old files from :
+rm -rvf /home/$USER/$COINDIR/{blocks,chainstate,backups,database,indexes}
+rm -rvf /home/$USER/$COINDIR/{*.conf,*.log,.lock,*.dat,*.pid,}
+
+echo dir after deletion
+pushd /home/$USER/$COINDIR/ && ln -s ../$COIN.conf && popd
+chown -R $USER:$USER /home/$USER/
+ls -alh /home/$USER/$COINDIR/
+
+echo starting service $COIN ...
+systemctl start $COIN && sleep 10 && systemctl status $COIN && $COINSTATUSCMD
