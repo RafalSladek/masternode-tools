f658be742ed74893000bdd87aa051e6b0c1f0664
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-17 07:54:48 +0100

    add explorer metrics for Chaincoin

---
+++ sentExplorerChaincoinMetrics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+source /usr/local/bin/ddhelper.sh
+
+host=$(hostname)
+coin="chc"
+role="masternode"
+username=$(whoami)
+coinexplorerurl=https://api.chaincoinexplorer.co.uk/getBlockchainInfo
+
+highestBlock=$(curl -sk $coinexplorerurl | jq .blocks)
+metricname="explorer.blocks"
+value=$highestBlock
+sentMetric $host $coin $metricname $value $role $username
