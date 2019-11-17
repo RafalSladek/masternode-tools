be76b401fae0ca75df89d642d17d80f2d612766d
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-18 00:08:19 +0100

    improve clean script for chc

--- cleanChaincoin.sh
+++ cleanChaincoin.sh
@@ -14,8 +14,8 @@ echo stoping service $COIN ...
 systemctl stop $COIN
 
 echo removing old files from :
-rm -rvf /home/$USER/$COINDIR/{blocks,chainstate,backups,database}
-rm -rvf /home/$USER/$COINDIR/{*.conf,*.log,.lock,*.dat,*.pid}
+rm -rvf /home/$USER/$COINDIR/{blocks,chainstate,backups,database,indexes}
+rm -rvf /home/$USER/$COINDIR/{*.conf,*.log,.lock,*.dat,*.pid,.walletlock}
 
 echo dir after deletion
 pushd /home/$USER/$COINDIR/ && ln -s ../$COIN.conf && popd
