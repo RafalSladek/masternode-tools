e5854d3f7c044e3056e8eb7f35512c1984a9551f
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-17 19:54:53 +0100

    refactor zen stats

--- sentZenMetrics.sh
+++ sentZenMetrics.sh
@@ -9,7 +9,9 @@ role="securenode"
 username=$(whoami)
 coindaemon="zend"
 coincli="zen-cli"
-myPublicIp=$(mypublicip)
+publicIp=$(mypublicip)
+coinexplorerurl=https://explorer.horizen.global/insight-api-zen/sync
+highestBlock=$(curl -sk $coinexplorerurl | jq .blockChainHeight)
 
 metricname="node.blocks"
 value=$(/usr/bin/$coincli getblockcount)
@@ -24,19 +26,22 @@ apiInfo=$(/usr/bin/curl --silent "https://$zensystembaseurl/api/nodes/$zensystem
 status=$(echo $apiInfo | jq -r .status)
 nodeIp4=$(echo $apiInfo | jq -r .ip4)
 nodefqdn=$(echo $apiInfo | jq -r .fqdn)
-
-success=0
-if [ 'up' == $status ] && [ $myPublicIp == $nodeIp4 ] && [ $nodefqdn == $FQDN ]; then
-    success=1
+value=0
+if [ 'up' == $status ] && [ $publicIp == $nodeIp4 ] && [ $nodefqdn == $FQDN ]; then
+    succvalueess=1
 fi
-sentMetric $host $coin $metricname $success $role $username
+sentMetric $host $coin $metricname $value $role $username
 
 metricname="node.status"
 netinfo=$(/usr/bin/$coincli getnetworkinfo)
 tlsVerified=$(echo $netinfo | jq -r .tls_cert_verified)
 nodePublicIp=$(echo $netinfo | jq -r .localaddresses[].address)
 success=0
-if [ 'true' == $tlsVerified ] && [ $myPublicIp == $nodePublicIp ]; then
+if [ 'true' == $tlsVerified ] && [ $publicIp == $nodePublicIp ]; then
     success=1
 fi
 sentMetric $host $coin $metricname $success $role $username
+
+metricname="explorer.blocks"
+value=$highestBlock
+sentMetric $host $coin $metricname $value $role $username
