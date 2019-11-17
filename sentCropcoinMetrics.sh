3342daf488b84c19bab8dfa247846fb5dc716acf
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-17 20:19:26 +0100

    refactor metrics for crop

--- sentCropcoinMetrics.sh
+++ sentCropcoinMetrics.sh
@@ -8,6 +8,7 @@ role="masternode"
 username=$(whoami)
 coindaemon="cropcoind"
 coincli=""
+publicIp=$(mypublicip)
 
 metricname="node.blocks"
 value=$(/usr/local/bin/$coindaemon getblockcount)
@@ -18,14 +19,13 @@ value=$(/usr/local/bin/$coindaemon getconnectioncount)
 sentMetric $host $coin $metricname $value $role $username
 
 metricname="node.enabled"
-pubilcip=$(curl -s ipecho.net/plain)
-value=$(/usr/local/bin/$coindaemon masternode list full | /bin/grep $pubilcip | /bin/grep -w ENABLED | /usr/bin/wc -l)
+value=$(/usr/local/bin/$coindaemon masternode list full | /bin/grep $publicIp | /bin/grep -w ENABLED | /usr/bin/wc -l)
 sentMetric $host $coin $metricname $value $role $username
 
 metricname="node.status"
-value=$(/usr/local/bin/$coindaemon masternode status | /bin/grep status | /usr/bin/awk -F' ' '{printf "%s",$3}' | /usr/bin/tr -d ",")
-success=0
-if [ '9' == $value ]; then
-    success=1
+status=$(/usr/local/bin/$coindaemon masternode status | jq .status)
+value=0
+if [ '9' == $status ]; then
+    value=0
 fi
-sentMetric $host $coin $metricname $success $role $username
+sentMetric $host $coin $metricname $value $role $username
