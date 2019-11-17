fa51722e82833732382e56c9f30f700e3927ef94
Rafal Sladek <rafal.sladek@gmail.com>
2019-11-17 20:05:52 +0100

    refactor metrics for arc

--- sentArcticoinMetrics.sh
+++ sentArcticoinMetrics.sh
@@ -8,6 +8,9 @@ role="masternode"
 username=$(whoami)
 coindaemon="arcticcoind"
 coincli="arcticcoin-cli"
+publicIp=$(mypublicip)
+coinexplorerurl=http://explorer.advtech.group/api/getblockcount
+highestBlock=$(curl -sk $coinexplorerurl)
 
 metricname="node.blocks"
 value=$(/usr/local/bin/$coincli getblockcount)
@@ -18,14 +21,17 @@ value=$(/usr/local/bin/$coincli getconnectioncount)
 sentMetric $host $coin $metricname $value $role $username
 
 metricname="node.enabled"
-publicip=$(curl -s ipecho.net/plain)
-value=$(/usr/local/bin/$coincli goldminenode list full | /bin/grep $publicip | /bin/grep -w ENABLED | /bin/grep -v PRE-ENABLED | /usr/bin/wc -l)
+value=$(/usr/local/bin/$coincli goldminenode list full $publicip | /bin/grep -w ENABLED | /bin/grep -v PRE-ENABLED | /usr/bin/wc -l)
 sentMetric $host $coin $metricname $value $role $username
 
 metricname="node.status"
-value=$(/usr/local/bin/$coincli goldminenode status | /bin/grep status | /usr/bin/awk -F'"' '{printf "%s",$4}' | /usr/bin/tr -d ",")
-success=0
-if [[ 'Goldminenode successfully started' == $value ]]; then
-    success=1
+status=$(/usr/local/bin/$coincli goldminenode status | /bin/grep status | /usr/bin/awk -F'"' '{printf "%s",$4}' | /usr/bin/tr -d ",")
+value=0
+if [[ 'Goldminenode successfully started' == $status ]]; then
+    value=1
 fi
-sentMetric $host $coin $metricname $success $role $username
+sentMetric $host $coin $metricname $value $role $username
+
+metricname="explorer.blocks"
+value=$highestBlock
+sentMetric $host $coin $metricname $value $role $username
