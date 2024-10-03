b8845fd85fc6746863fc905eb6071d9e0c5b8d47
Rafal Sladek <RafalSladek@users.noreply.github.com>
2024-10-03 13:07:45 +0200

    add remove logic to cleanup invalid links

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 TARGETPATH=/usr/local/bin
 
+
+
 for i in `ls -l | awk '{ if ( $1 ~ /x/ ) {print $NF}}'`;
 do
     if [ -x $i ]
@@ -16,6 +18,25 @@ do
     fi
 done
 
-cd $TARGETPATH && ls -lhaF | grep ^l
+cd zen
+
+for i in `ls -l | awk '{ if ( $1 ~ /x/ ) {print $NF}}'`;
+do
+    if [ -x $i ]
+    then
+        rm -rf $TARGETPATH/$i && echo "removed old $TARGETPATH/$i"
+        ln -s $SCRIPTPATH/$i $TARGETPATH/$i && echo "linked new $SCRIPTPATH/$i"
+    fi
+done
+
+cd $TARGETPATH
+echo "deleting invalid links..."
+find . -xtype l -exec rm {} \;
+
+echo "verify deletion of invalid links"
+find . -xtype l
+
+echo "list of exisintg links.."
+ls -lhaF | grep ^l
 
 source /usr/local/bin/tools.sh
